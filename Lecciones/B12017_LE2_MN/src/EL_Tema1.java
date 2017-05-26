f8d5afed6e72d502b87ee5a89eb394953e3b9a7e
melanny98 <melanny98@live.com>
2017-05-26 13:42:04 -0500

    Add files via upload

---
+++ Lecciones/B12017_LE2_MN/src/EL_Tema1.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author alum_tes_a
+ */
+public class EL_Tema1 {
+    public static void main(String args [])
+    
+    {
+   
+       int num;
+       num=0; 
+         while (num<=100)
+           {
+            if(num%10==0){
+                System.out.println(num);
+            }
+            num++;
+           }
+         
+         while(num>=0)
+           {
+            if(num%10==0){
+                System.out.println(num);
+            }
+            num--;
+    }
+    }
+
+}
+       
+
+
